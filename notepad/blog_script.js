173f11c6816dc50de97070228ac364ab67b224b7
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2023-08-15 23:56:30 +0200

    update structure, 2023-08-15

--- notepad/blog_script.js
+++ notepad/blog_script.js
@@ -1,35 +1,30 @@
-const postList = document.getElementById("post-list");
-
-// Function to sort posts by date in descending order
-function sortByDate(posts) {
-    return posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+async function fetchBlogData() {
+    const response = await fetch('blogData.json');
+    const data = await response.json();
+    return data;
 }
 
-// Replace this array with actual data from your blog
-const blogPosts = [
-    { title: "글을 쓰기 시작한 이유와 글쓰기", date: "2023-08-10" },
-    { title: "자의식에 대하여", date: "2023-08-11" },
-    { title: "인간은 언제나 완벽한 판단을 한다", date: "2023-08-12" },
-    { title: "민주주의는 항상 독재보다 옳은가", date: "2023-08-13" },
-    { title: "가벼운 이야기", date: "2023-08-14" },
-    // ... Add more posts here
-];
+(async () => {
+    const blogPosts = await fetchBlogData();
 
-// Sort posts by date
-sortByDate(blogPosts);
+    // Sort posts by date
+    blogPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-// Generate links to blog posts
-blogPosts.forEach(post => {
-    const postDate = new Date(post.date).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric"
+    const postList = document.getElementById("post-list");
+
+    // Generate links to blog posts
+    blogPosts.forEach(post => {
+        const postDate = new Date(post.date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        });
+
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.textContent = `${postDate} - ${post.title}`;
+        link.href = `texts/${post.date}.html`; // Change the filename format as needed
+        listItem.appendChild(link);
+        postList.appendChild(listItem);
     });
-
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.textContent = `${postDate} - ${post.title}`;
-    link.href = `texts/${post.date}.html`; // Change the filename format as needed
-    listItem.appendChild(link);
-    postList.appendChild(listItem);
-});
+})();
